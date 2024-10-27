b344c38a1987d222d3b9ff5b326de3f45f4cc22f
Iwatannaye Victor <iwatannayevictor@gmail.com>
2024-10-28 00:05:43 +0100

    added validators

--- src/frameworks/validator/validatorjs/validator.js
+++ src/frameworks/validator/validatorjs/validator.js
@@ -8,6 +8,7 @@ export default class Validator extends ValidatorInterface {
     constructor({ logger }) {
         super({ logger });
         Base.registerCustomRules();
+
         this.userValidator = new UserValidator({ logger });
         this.authValidator = new AuthValidator({ logger });
         this.videoValidator = new VideoValidator({ logger });
